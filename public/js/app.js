2d7c10e740f3c314d930ab3fbf319fff2c6b227f
joaopedro1997 <joaopedrodiasamaral@gmail.com>
2020-01-28 12:01:14 -0300

    algumas alterações irrelevantes

--- public/js/app.js
+++ public/js/app.js
@@ -51162,8 +51162,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\estoquezete_vue_laravel\estoquezete\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\estoquezete_vue_laravel\estoquezete\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /mnt/c/wamp64/www/estoquezete_vue_laravel/estoquezete/controle-estoque/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /mnt/c/wamp64/www/estoquezete_vue_laravel/estoquezete/controle-estoque/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
